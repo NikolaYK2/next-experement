e46562d02485c9a416c10318c4b33359e4599de2
k2 <izmail.k2@gmail.com>
2024-10-02 14:45:31 +0300

    feat: config for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'rickandmortyapi.com',
+                pathname: '/api/character/avatar/**'//** - означает что будет какая угодна картинка
+            }
+        ]
+    }
 };
 
 export default nextConfig;
