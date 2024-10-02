933ca067f435ca904184918f53d1208336cff4b0
k2 <izmail.k2@gmail.com>
2024-10-02 14:50:47 +0300

    feat: add CharacterCard

---
+++ components/ui/characterCard/CharacterCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from "next/image";
+import {CharactersType} from "@/assets/common/hooks/useCharacters";
+import s from "@/pages/characters/index.module.scss";
+
+export const CharacterCard = ({name, image, id}: CharactersType) => {
+  return (
+    <div className={s.blockItem}>
+      <div>{name}</div>
+      <Image src={image} alt={`Picture of ${name}`} width={300} height={200}/>
+
+    </div>
+  );
+};
