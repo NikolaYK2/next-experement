f2a1c2dfbf1f7e85190aa577ce1881fef3ff2214
k2 <izmail.k2@gmail.com>
2024-10-02 14:44:21 +0300

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import {getLayout} from "@/components/ui/layout/Layout";
+
+const NotFound = () => {
+  return <div>404 NOT FOUND</div>
+}
+
+NotFound.getLayout = getLayout
+export default NotFound
+
