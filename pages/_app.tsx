5bdff21ed00d2849b7be491a8cf5082618bc5ffc
k2 <izmail.k2@gmail.com>
2024-10-02 14:42:25 +0300

    feat: add Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+//сюда подключаются глобальные стили
+import type {ReactElement, ReactNode} from 'react'
+import type {NextPage} from 'next'
+import type {AppProps} from 'next/app'
+import '@nikolajk2/lib-insta-leaders/css'
+//подключение layout
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
