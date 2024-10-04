95f2010c94475ae883c57feabb57e800c4383cc1
k2 <izmail.k2@gmail.com>
2024-10-04 21:24:11 +0300

    refactor: remove id prop

--- components/ui/characterCard/CharacterCard.tsx
+++ components/ui/characterCard/CharacterCard.tsx
@@ -3,12 +3,11 @@ import Image from "next/image";
 import {CharactersType} from "@/assets/common/hooks/useCharacters";
 import s from "./CharacterCard.module.scss";
 
-export const CharacterCard = ({name, image, id}: CharactersType) => {
+export const CharacterCard = ({name, image}: CharactersType) => {
   return (
     <div className={s.blockItem}>
       <div>{name}</div>
       <Image src={image} alt={`Picture of ${name}`} width={300} height={200}/>
-
     </div>
   );
 };
