5e003987f29e1ff0e6a2a415f9969bf8427e5d81
k2 <izmail.k2@gmail.com>
2024-10-04 13:26:51 +0300

    feat: add ReactQuery

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,29 @@
 import "@/styles/globals.css";
 //сюда подключаются глобальные стили
-import type {ReactElement, ReactNode} from 'react'
+import {ReactElement, ReactNode, useState} from 'react'
 import type {NextPage} from 'next'
 import type {AppProps} from 'next/app'
-import '@nikolajk2/lib-insta-leaders/css'
+import {HydrationBoundary, QueryClient, QueryClientProvider} from '@tanstack/react-query'
 //подключение layout
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
 export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
+  const [queryClient] = useState(() => new QueryClient())
+
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  )
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
 }
