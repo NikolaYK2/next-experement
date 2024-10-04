569fc6b12487431d909ee25bcd0b9c121608f148
k2 <izmail.k2@gmail.com>
2024-10-04 15:41:01 +0300

    feat: useLoader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import "@/styles/globals.css";
-//сюда подключаются глобальные стили
 import {ReactElement, ReactNode, useState} from 'react'
 import type {NextPage} from 'next'
 import type {AppProps} from 'next/app'
 import {HydrationBoundary, QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {useLoader} from "@/assets/common/hooks/useLodaer";
+import '../styles/nprogress.css'
+
 //подключение layout
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
 export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient())
-
+  useLoader()
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
@@ -24,6 +29,3 @@ export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
   )
 }
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
