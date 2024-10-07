26c0f9699730116bf3f1ae16140fef4f1b0aa183
k2 <izmail.k2@gmail.com>
2024-10-07 21:26:14 +0300

    feat: add Private

---
+++ pages/private/index.tsx
@@ -0,0 +1,14 @@
+import {getLayout} from "@/components/ui/layout/baseLayout/BaseLayout";
+import {LoginNavigate} from "@/assets/common/hoc/LoginNavigate";
+
+
+const Private = () => {
+
+  return <LoginNavigate>Private PAGE</LoginNavigate>
+}
+
+Private.getLayout = getLayout
+export default Private
+
+
+
